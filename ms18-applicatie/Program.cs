cdaab0483063580a7f627bdfbe8b73525ccb632d
Frits <0765612@hr.nl>
2024-02-17 15:10:46 +0100

    Update Program.cs

--- ms18-applicatie/Program.cs
+++ ms18-applicatie/Program.cs
@@ -169,6 +169,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseHttpsRedirection();
 }
 
 
@@ -179,7 +180,7 @@ app.UseSwaggerUI(options =>
 });
 
 app.UseSwagger();
-app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 app.UseRouting();
 app.UseCors("MyAllowSpecificOrigins");
